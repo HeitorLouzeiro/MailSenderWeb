08e5b4958cdf25623923195097f09bf2a95143e5
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2024-02-13 22:54:10 -0300

    Added templates and views for sending email.

--- mailSenderWeb/views.py
+++ mailSenderWeb/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import HttpResponse, render
+from django.contrib import messages
+from django.shortcuts import HttpResponse, redirect, render
 
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Hello, World!')
+    template_name = 'index.html'
+    if request.method == 'POST':
+        # Get the data from the form
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        file = request.FILES.get('file')
+        message = request.POST.get('message')
+
+        if email == '' or subject == '' or message == '':
+            messages.info(
+                request, 'Please fill in email, subject and message fields')
+            return render(request, template_name)
+
+        # Return a success message
+        return HttpResponse(email + ' ' + subject + ' ' + message)
+    return render(request, template_name)
--- settings/settings.py
+++ settings/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from django.contrib.messages import constants
 import os
 from pathlib import Path
 
@@ -119,10 +120,19 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'mailSenderWeb/static'
+    BASE_DIR / 'mailSenderWeb/templates/static'
 ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MESSAGE_TAGS = {
+    constants.ERROR: 'error',
+    constants.WARNING: 'warning',
+    constants.DEBUG: 'info',
+    constants.SUCCESS: 'success',
+    constants.INFO: 'info',
+}
